d8d49700aa18501dcc68482e91e9dc01ddc02c3e
Harnihkeh <Abdulraheem Hameedah Aderonke>
2022-09-08 19:19:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
-#include <unistd.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+#include <stdio.h>                                                            
+#include <unistd.h>                                                           
+/**                                                                           
+ * main - entry point                                                         
+ *                                                                            
+ * Description: prints and that piece..                                       
+ *Return: 1 if sucessful                                                      
  */
+
 int main(void)
-{
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n")
-return (1)
+{                                                                             
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\
+n", 59);                                                                      
+        return (1);                                                           
 }
