a329d37b3144492a22f3026bc997226d1fc32bb2
Harnihkeh <Abdulraheem Hameedah Aderonke>
2022-09-13 11:59:05 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * main - prints Putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * main - print putchar
+ * Return: always 0
  */
 
 int main(void)
@@ -21,4 +18,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
